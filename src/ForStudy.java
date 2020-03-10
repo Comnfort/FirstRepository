d50952daf3dcb47c6942812c1ddea40576f89e79
Comnfort <comfortkrit@icloud.com>
2020-03-10 23:06:44 +0300

    init

--- src/ForStudy.java
+++ src/ForStudy.java
@@ -1,7 +1,7 @@
 public class ForStudy {
     public static void main(String[] args) {
         System.out.println("New World");
-        System.out.println("");
+        System.out.println("N W");
     }
 }
 
